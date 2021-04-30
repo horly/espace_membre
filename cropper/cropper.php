c82520ce91c7d6dc3122d74de0be2d22a1a4b3dd
Andelo Mata <andmatboy@gmail.com>
2021-04-30 22:22:33 +0100

    Add files via upload

---
+++ cropper/cropper.php
@@ -0,0 +1,93 @@
+
+    <div class="img-container1">
+        <img id="image" src="" alt="Picture">
+    </div>
+
+    <label>&nbsp;</label>
+    <label class="btn btn-light border btn-upload" for="inputImage" title="Upload image file">
+        <input type="file" class="sr-only" id="inputImage" name="file" accept=".jpg,.jpeg,.png,.gif,.bmp,.tiff">
+        Importer...
+    </label>
+    <br>
+    &nbsp;
+    <div class="docs-buttons">
+
+        <!-- <div class="btn-group">
+           <button type="button" class="btn btn-primary" data-method="setDragMode" data-option="move" title="Bouger">
+             <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+               <span class="fa fa-arrows"></span>
+             </span>
+           </button>
+           <button type="button" class="btn btn-primary" data-method="setDragMode" data-option="crop" title="Rogner">
+             <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+               <span class="fa fa-crop"></span>
+             </span>
+           </button>
+         </div>-->
+        &nbsp;&nbsp;
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary" data-method="zoom" data-option="0.1" title="Zoom Avant">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false" >
+        <span class="fa fa-search-plus"></span>
+      </span>
+            </button>
+            <button type="button" class="btn btn-primary" data-method="zoom" data-option="-0.1" title="Zoom Arrière">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-search-minus"></span>
+      </span>
+            </button>
+        </div>
+
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary" data-method="move" data-option="-10" data-second-option="0" title="Déplacer à gauche">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-arrow-left">&nbsp;</span>
+      </span>
+            </button>
+            <button type="button" class="btn btn-primary" data-method="move" data-option="10" data-second-option="0" title="Déplacer à droite">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-arrow-right"></span>
+      </span>
+            </button>
+        </div>
+
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary" data-method="move" data-option="0" data-second-option="-10" title="Déplacer en haut">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-arrow-up"></span>
+      </span>
+            </button>
+            <button type="button" class="btn btn-primary" data-method="move" data-option="0" data-second-option="10" title="Déplacer en bas">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-arrow-down"></span>
+      </span>
+            </button>
+        </div>
+
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary" data-method="rotate" data-option="-45" title="Tourne à gauche">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-rotate-left">&nbsp;</span>
+      </span>
+            </button>
+            <button type="button" class="btn btn-primary" data-method="rotate" data-option="45" title="Tourne à droite">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-rotate-right"></span>
+      </span>
+            </button>
+        </div>
+
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary" data-method="scaleX" data-option="-1" title="Retourner horizontalement">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-arrows-h"></span>
+      </span>
+            </button>
+            <button type="button" class="btn btn-primary" data-method="scaleY" data-option="-1" title="Retourner verticalement">
+      <span class="docs-tooltip" data-toggle="tooltip" data-animation="false">
+        <span class="fa fa-arrows-v">&nbsp;&nbsp;</span>
+      </span>
+            </button>
+        </div>
+
+    </div>
